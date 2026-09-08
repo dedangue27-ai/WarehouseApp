76b6c2989651ff92be84aa64a73348a4b56162c5
dedangue27-ai <dedangue27@gmail.com>
2026-09-08 17:44:52 +0100

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -44,7 +44,7 @@ namespace WarehouseApp
             
             // كود التحقق من التحديثات
             AutoUpdater.ReportErrors = true;
-            AutoUpdater.Start("https://raw.githubusercontent.com/dedangue27-ai/WarehouseApp/main/update.xml");
+            AutoUpdater.Start("https://raw.githubusercontent.com/dedangue27-ai/WarehouseApp/refs/heads/main/update.xml");
         }
 
         // دالة لفتح نافذة التعديل عند النقر المزدوج على صف المنتج
